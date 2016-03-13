40c6bf219a2c56ee092d0abd6ba52c2b8ba03a07
senior209 <178561618@qq.com>
2016-03-13 17:55:08 +0800

    te
    te

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -12,8 +12,6 @@
    <button type="button" onclick="toCreate()">发布新博客</button>
 </div>
 @foreach($articles as $article)
-
-	
 <h1 style="text-align: center"><a  href="{{url('articles',$article->id)}}">	{{$article->title}} </a></h1>
     <h3 style="text-align: center" >{{$article->published_at}}</h3>
 
